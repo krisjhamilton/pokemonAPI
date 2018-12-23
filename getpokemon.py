2fd29a23ade3a006be07dcd6dea49fd0e115a884
krisjhamilton <krisjhamilton@gmail.com>
2018-12-24 07:46:35 +1300

    Logging

--- getpokemon.py
+++ getpokemon.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
+import requests
 from pokemontcgsdk import Card
 # from pokemontcgsdk import Set
 # from pokemontcgsdk import Type
 # from pokemontcgsdk import Supertype
 # from pokemontcgsdk import Subtype
 
-cards = Card.where(page=5, pageSize=100)
-print(card)
+# requests.packages.urllib3.disable_warnings()
+
+import traceback
+
+#This line opens a log file
+log = open("log.txt", "w")
+
+try:
+    # some code
+    # Below line will print any print to log file as well.
+    session = requests.Session()
+    session.verify = False
+
+    cards = Card.where(page=5, pageSize=100)
+    print(card)
+    print("Creating DB Connection", file = log)
+except Exception:
+    traceback.print_exc(file=log)
+
+
